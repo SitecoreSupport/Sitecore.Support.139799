da1a42431433a0b17d76ccefbbd5ad5f378be30c
Viktor Kucherov <vkucherov@live.ru>
2017-04-13 13:11:31 +0800

    no message

---
+++ src/Sitecore.Support.139799/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.139799")]
+[assembly: AssemblyProduct("Sitecore.Support.139799")]
+[assembly: ComVisible(false)]
